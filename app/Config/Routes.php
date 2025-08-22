35bfaa2b98f60372bfb8c8daa355b85740ebe656
Leunhart <jessicajeffry991@gmail.com>
2025-08-22 16:19:25 +0700

    route paginated version untuk tampilan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,28 +9,36 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->get('/agenda', 'Home::agenda');
+$routes->get('/agenda_paginated', 'Home::agenda_paginated');
 $routes->get('/visi-misi', 'Home::visi_misi');
 
 $routes->get('/asisten', 'Home::asisten');
+$routes->get('/asisten_paginated', 'Home::asisten_paginated');
 $routes->get('/asisten_admin', 'Home::asisten_admin');
 
 $routes->get('/jadwal', 'Home::jadwal');
 $routes->get('/jadwal-list', 'Home::jadwal_card');
+$routes->get('/jadwal_paginated', 'Home::jadwal_paginated');
 $routes->get('/jadwal_admin', 'Home::jadwal_admin');
 
 $routes->get('/penelitian-proyek', 'Home::penelitian_proyek');
+$routes->get('/penelitian-proyek_paginated', 'Home::penelitian_proyek_paginated');
 $routes->get('/penelitian-proyek_admin', 'Home::penelitian_proyek_admin');
 
 $routes->get('/publikasi-ilmiah', 'Home::publikasi_ilmiah');
+$routes->get('/publikasi-ilmiah_paginated', 'Home::publikasi_ilmiah_paginated');
 $routes->get('/publikasi-ilmiah_admin', 'Home::publikasi_ilmiah_admin');
 
 $routes->get('/galeri', 'Home::galeri');
+$routes->get('/galeri_paginated', 'Home::galeri_paginated');
 $routes->get('/galeri_admin', 'Home::galeri_admin');
 
 $routes->get('/repositori', 'Home::repositori');
+$routes->get('/repositori_paginated', 'Home::repositori_paginated');
 $routes->get('/repositori_admin','Home::repositori_admin');
 
 $routes->get('/rekrutmen', 'Home::rekrutmen');
+$routes->get('/rekrutmen_paginated', 'Home::rekrutmen_paginated');
 $routes->get('/rekrutmen_admin','Home::rekrutmen_admin');
 
 // routes API Content
