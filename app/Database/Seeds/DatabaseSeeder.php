6198e70004d26d09535a0a99434758e864774c53
Leunhart <jessicajeffry991@gmail.com>
2025-08-22 16:16:51 +0700

    seeder sebagai data dummy ditambah untuk test pagination

--- app/Database/Seeds/DatabaseSeeder.php
+++ app/Database/Seeds/DatabaseSeeder.php
@@ -21,6 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call('GaleriUmumSeeder');
         $this->call('ModulPraktikumSeeder');
         $this->call('PesertaPraktikumSeeder');
-         $this->call('VisiMisiSeeder'); 
+        $this->call('VisiMisiSeeder'); 
     }
 }
